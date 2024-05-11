16732614924e38a07c568aa609ffe5095360e8f6
viktoriyafv <152386470+viktoriyafv@users.noreply.github.com>
2024-05-11 15:04:34 +0300

    Адаптивные декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /*сюда добалвяют скрипт*/
+const navMain = document.querySelector('.main-header__container');
+const navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-header__container--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-header__container--closed')) {
+    navMain.classList.remove('main-header__container--closed');
+    navMain.classList.add('main-header__container--opened');
+  } else {
+    navMain.classList.add('main-header__container--closed');
+    navMain.classList.remove('main-header__container--opened');
+  }
+});

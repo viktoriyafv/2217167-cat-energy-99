554d8a93a3f19ad3aa1caed1fd35c692db76c369
viktoriyafv <152386470+viktoriyafv@users.noreply.github.com>
2024-05-21 09:51:02 +0300

    JS слайдер (#70)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,12 @@
 /*сюда добалвяют скрипт*/
 const navMain = document.querySelector('.main-header__container');
 const navToggle = document.querySelector('.main-header__toggle');
+const slider = document.querySelector('.slider');
+const curtain = slider.querySelector('.slider__thumb');
+const sliderStyles = getComputedStyle(slider);
+let curtainPlaceStart;
+let clientX;
+
 
 navMain.classList.remove('main-header__container--nojs');
 
@@ -13,3 +19,23 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-header__container--opened');
   }
 });
+
+window.addEventListener('pointerup', stopTheCurtainShifting);
+curtain.addEventListener('pointerdown', startTheCurtainShifting);
+
+function startTheCurtainShifting (event) {
+  curtainPlaceStart = +(sliderStyles.getPropertyValue('--curtain-place'));
+  clientX = event.clientX;
+  window.addEventListener('pointermove', shiftТheСurtain);
+}
+
+function shiftТheСurtain (event) {
+  const deltaX = event.clientX - clientX;
+  const cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth;
+  const curtainPlace = Math.min(Math.max(cursorPlace, 0), 1);
+  slider.style.setProperty('--curtain-place', `${curtainPlace}`);
+}
+
+function stopTheCurtainShifting () {
+  window.removeEventListener('pointermove', shiftТheСurtain);
+}

3a829d91a0c2acffbb9901c4f7d89bcdd3c26091
viktoriyafv <152386470+viktoriyafv@users.noreply.github.com>
2024-05-22 10:43:59 +0300

    Исправление JS

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,24 +1,9 @@
 /*сюда добалвяют скрипт*/
-const navMain = document.querySelector('.main-header__container');
-const navToggle = document.querySelector('.main-header__toggle');
 const slider = document.querySelector('.slider');
 const curtain = slider.querySelector('.slider__thumb');
 const sliderStyles = getComputedStyle(slider);
 let curtainPlaceStart;
 let clientX;
-
-
-navMain.classList.remove('main-header__container--nojs');
-
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-header__container--closed')) {
-    navMain.classList.remove('main-header__container--closed');
-    navMain.classList.add('main-header__container--opened');
-  } else {
-    navMain.classList.add('main-header__container--closed');
-    navMain.classList.remove('main-header__container--opened');
-  }
-});
 
 window.addEventListener('pointerup', stopTheCurtainShifting);
 curtain.addEventListener('pointerdown', startTheCurtainShifting);

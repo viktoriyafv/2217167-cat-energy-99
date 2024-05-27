f240c822b4dfe77dcffee1235bef224cd52dec55
viktoriyafv <152386470+viktoriyafv@users.noreply.github.com>
2024-05-27 21:34:27 +0300

    partnerShip.js

--- source/scripts/menu.js
+++ source/scripts/menu.js
@@ -1,10 +1,8 @@
 /*сюда добалвяют скрипт*/
 const navMain = document.querySelector('.main-header__container');
 const navToggle = document.querySelector('.main-header__toggle');
-const partnerShip = document.querySelector('.partnership');
 
 navMain.classList.remove('main-header__container--nojs');
-partnerShip.classList.remove('partnership--nojs');
 
 navToggle.onclick = function () {
   navMain.classList.toggle('main-header__container--opened');
